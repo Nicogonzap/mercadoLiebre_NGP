6f5a731b108493cfddd7c159fcc63801b3503dcd
Nicogonzap <nicolasgonzap@gmail.com>
2021-10-25 19:17:29 -0300

    Maqueta del sitio hasta clase 10

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const app = express ();
+const path = require('path');
+const port = 3000;
+
+const pathViews = path.join (__dirname,'/views/')
+app.use(express.static('public'));
+
+
+
+app.listen (port, () => console.log ('Servidor corriendo en el puert') + port)
+
+app.get('/', (req, res) => {
+    res.sendFile(pathViews+'home.html')
+})
+
+
+/*
+Caja de input copiada de Meli
+<input id= "buscador" type="text" class="nav-search-input" aria-label="Ingresá lo que quieras encontrar" name="as_word" placeholder="Buscar productos, marcas y más…" maxlength="120" autofocus="" autocapitalize="off" autocorrect="off" spellcheck="false" autocomplete="off" tabindex="2">
+*/
