46f6977db19ae7ea94d73e5854717d41223ad25f
Nicogonzap <nicolasgonzap@gmail.com>
2021-11-10 19:47:26 -0300

    Agregada la config para Heroku

--- app.js
+++ app.js
@@ -9,23 +9,21 @@ app.use(express.static('public'));
 
 
 
-app.listen (port, () => console.log ('Servidor corriendo en el puerto ' + port))
+app.listen (process.env.PORT || port, () => {
+ console.log ('Servidor corriendo en el puerto ' + port)
+});
 
 app.get('/', (req, res) => {
-    res.sendFile(pathViews+'home.html')
-})
+    res.sendFile(pathViews+'home.html');
+});
 
 app.get('/register', (req, res) => {
-    res.sendFile(pathViews+'register.html')
-})
+    res.sendFile(pathViews+'register.html');
+});
 app.get('/login', (req, res) => {
-    res.sendFile(pathViews+'login.html')
-})
+    res.sendFile(pathViews+'login.html');
+});
 //Esta ruta de tipo get se utiliza para redireccionar el formulario al home
 app.post("/registro", (req, res) => {
     res.redirect ("/");
-})
-/*
-Caja de input copiada de Meli
-<input id= "buscador" type="text" class="nav-search-input" aria-label="Ingresá lo que quieras encontrar" name="as_word" placeholder="Buscar productos, marcas y más…" maxlength="120" autofocus="" autocapitalize="off" autocorrect="off" spellcheck="false" autocomplete="off" tabindex="2">
-*/
+});
